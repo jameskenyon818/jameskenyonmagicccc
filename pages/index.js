53b7375a2088191dc36f4ed5468145749ff2f0ff
jameskenyon818 <Jameskenyonmagic@gmail.com>
2025-05-10 17:39:25 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>James Kenyon Magic</h1>
+      <p>Professional magician for trade shows, events, and parties.</p>
+    </main>
+  );
+}
